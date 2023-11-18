db9d20fed8bcb2a0a06e1dddc176a269a60c8bb0
Luis Victoria <x@lambda.lv>
2023-11-18 07:37:39 +0000

    Add  to dependencies

--- configuration.nix
+++ configuration.nix
@@ -69,7 +69,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    go
+    neovim
     wget
   ];
 
